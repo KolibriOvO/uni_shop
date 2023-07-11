64eaebc85e52d62eb69cca7f254d44c4859f6524
kolibri <vigilanteovo@gmail.com>
2023-07-11 11:47:31 +0800

    商品列表

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		Goods_list
+		<view class="goods-list">
+			<view v-for="(item, index) in goodsList" :key="index" @click="gotoDetail(item)">
+				<my-goods :goods="item"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +12,88 @@
 	export default {
 		data() {
 			return {
-				
-			};
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10,
+					goodsList: [],
+					total: 0,
+					defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+					isloading: false
+				};
+			}
+		},
+		methods: {
+			async getGoodsList(cb) {
+				this.isloading = true
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isloading = false
+				cb && cb()
+
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			gotoDetail(item) {
+			  uni.navigateTo({
+			    url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+			  })
+			}
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			this.getGoodsList()
+		},
+		onReachBottom() {
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+			if (this.isloading) return
+
+			this.queryObj.pagenum += 1
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+
+			this.getGoodsList(() => uni.stopPullDownRefresh())
 		}
-	}
 </script>
 
 <style lang="scss">
+	.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
 
+		.goods-item-left {
+			margin-right: 5px;
+
+			.goods-pic {
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+			}
+
+			.goods-price {
+				font-size: 16px;
+				color: #c00000;
+			}
+		}
+	}
 </style>
