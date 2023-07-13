d6055403f915bed57cc7c64a070542dc36552aba
kolibri <vigilanteovo@gmail.com>
2023-07-13 12:31:54 +0800

    商品组件

--- uni_modules/my-goods/components/my-goods/my-goods.vue
+++ uni_modules/my-goods/components/my-goods/my-goods.vue
@@ -1,16 +1,15 @@
 <template>
 	<view class="goods-item">
-
 		<view class="goods-item-left">
+			<radio :checked="goods.goods_state" color="#C00000" v-if="showRadio" @click="radioClickHandler"></radio>
 			<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
 		</view>
 
 		<view class="goods-item-right">
-
-			<view class="goods-name">{{goods.goods_name}}</view>
+			<view class="goods-name">{{ goods.goods_name }}</view>
 			<view class="goods-info-box">
-
-				<view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+				<view class="goods-price">￥{{ goods.goods_price | tofixed }}</view>
+				<uni-number-box :min="1" :value="goods.goods_count" @change="numChangeHandler" v-if="showNum"></uni-number-box>
 			</view>
 		</view>
 	</view>
@@ -20,21 +19,43 @@
 		props: {
 			goods: {
 				type: Object,
-				defaul: {},
+				defaul: {}
+			},
+			showRadio: {
+				type: Boolean,
+				default: false,
+			},
+			showNum: {
+				type: Boolean,
+				default: false,
 			},
 		},
 		data() {
 			return {
-				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		},
+		methods: {
+			radioClickHandler() {
+				this.$emit('radio-change', {
+					goods_id: this.goods.goods_id,
+					goods_state: !this.goods.goods_state
+				})
+			},
+			numChangeHandler(val) {
+				this.$emit('num-change', {
+					goods_id: this.goods.goods_id,
+					goods_count: +val
+				})
 			}
 		},
 		filters: {
 			// 把数字处理为带两位小数点的数字
 			tofixed(num) {
-				return Number(num).toFixed(2)
+				return Number(num).toFixed(2);
 			}
 		}
-	}
+	};
 </script>
 <style lang="scss">
 	.goods-item {
@@ -44,6 +65,9 @@
 
 		.goods-item-left {
 			margin-right: 5px;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
 
 			.goods-pic {
 				width: 100px;
@@ -51,20 +75,27 @@
 				display: block;
 			}
 		}
+	}
 
-		.goods-item-right {
+	.goods-item-right {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: space-between;
+
+		.goods-name {
+			font-size: 13px;
+		}
+
+		.goods-info-box {
 			display: flex;
-			flex-direction: column;
+			align-items: center;
 			justify-content: space-between;
+		}
 
-			.goods-name {
-				font-size: 13px;
-			}
-
-			.goods-price {
-				font-size: 16px;
-				color: #c00000;
-			}
+		.goods-price {
+			font-size: 16px;
+			color: #c00000;
 		}
 	}
 </style>
