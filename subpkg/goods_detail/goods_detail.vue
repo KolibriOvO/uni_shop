79684a4313021200c1f2beb7a440d073615008f8
kolibri <vigilanteovo@gmail.com>
2023-07-13 12:31:36 +0800

    商品详情

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -21,175 +21,165 @@
 		<rich-text :nodes="goods_info.goods_introduce"></rich-text>
 
 		<view class="goods_nav">
-			<uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick" @buttonClick="buttonClick" />
+			<uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick"
+				@buttonClick="buttonClick" />
 		</view>
 	</view>
 </template>
 
 <script>
-import {
-	mapState,
-	mapMutations,
-	mapGetters
-} from 'vuex'
-import {
-	computed
-} from "vue"
+	import {
+		mapState,
+		mapMutations,
+		mapGetters
+	} from 'vuex'
 
 
-export default {
-	data() {
-		return {
-			goods_info: {},
-			options: [{
-				icon: 'shop',
-				text: '店铺'
-			}, {
-				icon: 'cart',
-				text: '购物车',
-				info: 2
-			}],
-			buttonGroup: [{
-					text: '加入购物车',
-					backgroundColor: '#ff0000',
-					color: '#fff'
-				},
-				{
-					text: '立即购买',
-					backgroundColor: '#ffa200',
-					color: '#fff'
-				}
-			]
-		}
-	};
-},
-methods: {
-		...mapMutations('m_cart', ['addToCart']),
-		async getGoodsDetail(goods_id) {
-			const {
-				data: res
-			} = await uni.$http.get('/api/public/v1/goods/detail', {
-				goods_id
-			})
-			if (res.meta.status !== 200) return uni.$showMsg()
-
-			res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;" ')
-				.replace(/webp/g, 'jpg')
-			this.goods_info = res.message
+	export default {
+		computed: {
+			...mapState('m_cart', []),
+			...mapGetters('m_cart', ['total'])
 		},
-		preview(index) {
-			uni.previewImage({
-				current: index,
-				urls: this.goods_info.pics.map(item => item.pics_big)
-			})
+		data() {
+			return {
+				goods_info: {},
+				options: [{
+					icon: 'shop',
+					text: '店铺'
+				}, {
+					icon: 'cart',
+					text: '购物车',
+					info: 2
+				}],
+				buttonGroup: [{
+						text: '加入购物车',
+						backgroundColor: '#ff0000',
+						color: '#fff'
+					},
+					{
+						text: '立即购买',
+						backgroundColor: '#ffa200',
+						color: '#fff'
+					}
+				]
+			}
 		},
-		onClick(e) {
-			if (e.content.text === '购物车') {
-				uni.switchTab({
-					url: '/pages/cart/cart'
+		methods: {
+			...mapMutations('m_cart', ['addToCart']),
+			async getGoodsDetail(goods_id) {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/detail', {
+					goods_id
 				})
-			}
-		},
-		buttonClick(e) {
-			// 1. 判断是否点击了 加入购物车 按钮
-			if (e.content.text === '加入购物车') {
+				if (res.meta.status !== 200) return uni.$showMsg()
 
-				// 2. 组织一个商品的信息对象
-				const goods = {
-					goods_id: this.goods_info.goods_id, // 商品的Id
-					goods_name: this.goods_info.goods_name, // 商品的名称
-					goods_price: this.goods_info.goods_price, // 商品的价格
-					goods_count: 1, // 商品的数量
-					goods_small_logo: this.goods_info.goods_small_logo, // 商品的图片
-					goods_state: true // 商品的勾选状态
-				}
-
-				// 3. 通过 this 调用映射过来的 addToCart 方法，把商品信息对象存储到购物车中
-				this.addToCart(goods)
-
-			}
-		}
-	},
-	computed() {
-		...mapGetters('m_cart', ['total']),
-	},
-	watch: {
-		// 定义 total 侦听器，指向一个配置对象
-		total: {
-			// handler 属性用来定义侦听器的 function 处理函数
-			handler(newVal) {
-				const findResult = this.options.find(x => x.text === '购物车')
-				if (findResult) {
-					findResult.info = newVal
+				res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;" ')
+					.replace(/webp/g, 'jpg')
+				this.goods_info = res.message
+			},
+			preview(index) {
+				uni.previewImage({
+					current: index,
+					urls: this.goods_info.pics.map(item => item.pics_big)
+				})
+			},
+			onClick(e) {
+				if (e.content.text === '购物车') {
+					uni.switchTab({
+						url: '/pages/cart/cart'
+					})
 				}
 			},
-			// immediate 属性用来声明此侦听器，是否在页面初次加载完毕后立即调用
-			immediate: true
+			buttonClick(e) {
+				if (e.content.text === '加入购物车') {
+					const goods = {
+						goods_id: this.goods_info.goods_id, // 商品的Id
+						goods_name: this.goods_info.goods_name, // 商品的名称
+						goods_price: this.goods_info.goods_price, // 商品的价格
+						goods_count: 1, // 商品的数量
+						goods_small_logo: this.goods_info.goods_small_logo, // 商品的图片
+						goods_state: true // 商品的勾选状态
+					}
+
+					this.addToCart(goods)
+				}
+			}
+		},
+		watch: {
+			total: {
+				handler(newVal) {
+					const findResult = this.options.find(x => x.text === '购物车')
+					if (findResult) {
+						findResult.info = newVal
+					}
+				},
+				immediate: true
+			}
+		},
+		onLoad(options) {
+			const goods_id = options.goods_id
+			this.getGoodsDetail(goods_id)
 		}
 	}
-onLoad(options) {
-	const goods_id = options.goods_id
-	this.getGoodsDetail(goods_id)
-}
-}
 </script>
 
 <style lang="scss">
-swiper {
-	height: 750rpx;
+	swiper {
+		height: 750rpx;
 
-	image {
-		width: 100%;
-		height: 100%;
-	}
-}
-
-.goods-info-box {
-	padding: 10px;
-	padding-right: 0;
-
-	.price {
-		color: #c00000;
-		font-size: 18px;
-		margin: 10px 0;
+		image {
+			width: 100%;
+			height: 100%;
+		}
 	}
 
-	.goods-info-body {
-		display: flex;
-		justify-content: space-between;
+	.goods-info-box {
+		padding: 10px;
+		padding-right: 0;
 
-		.goods-name {
-			font-size: 13px;
-			padding-right: 10px;
+		.price {
+			color: #c00000;
+			font-size: 18px;
+			margin: 10px 0;
 		}
 
-		.favi {
-			width: 120px;
-			font-size: 12px;
+		.goods-info-body {
 			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			border-left: 1px solid #efefef;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+				padding-right: 10px;
+			}
+
+			.favi {
+				width: 120px;
+				font-size: 12px;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				border-left: 1px solid #efefef;
+				color: gray;
+			}
+		}
+
+		.express {
+			margin: 10px 0;
+			font-size: 12px;
 			color: gray;
 		}
 	}
 
-	.express {
-		margin: 10px 0;
-		font-size: 12px;
-		color: gray;
+	.goods-detail-container {
+		padding-bottom: 50px;
 	}
-}
 
-.goods-detail-container {
-	padding-bottom: 50px;
-}
-
-.goods_nav {
-	position: fixed;
-	bottom: 0;
-	left: 0;
-	width: 100%;
-}
+	.goods_nav {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+	}
 </style>
