5b0f7903c17c4e466e9e4fd50e98925ad36e8509
kolibri <vigilanteovo@gmail.com>
2023-07-14 09:39:31 +0800

    add:空界面

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,14 +1,26 @@
 <template>
-	<view>
+	<view class="cart-container" v-if="cart.length !== 0">
+		<my-address></my-address>
 		<view class="cart-title">
 			<uni-icons type="shop" size="18"></uni-icons>
 			<text class="cart-title-text">购物车</text>
 		</view>
 
-		<block v-for="(goods, i) in cart" :key="i">
-			<my-goods :goods="goods" :show-radio="true" :show-num="true" @radio-change="radioChangeHandler"
-				@num-change="numberChangeHandler"></my-goods>
-		</block>
+		<uni-swipe-action>
+			<block v-for="(goods, index) in cart" :key="index">
+				<uni-swipe-action-item :right-options="options" @click="swipeActionClickHandler(goods)">
+					<my-goods :goods="goods" :show-radio="true" :show-num="true" @radio-change="radioChangeHandler"
+						@num-change="numberChangeHandler"></my-goods>
+				</uni-swipe-action-item>
+			</block>
+		</uni-swipe-action>
+		<my-settle></my-settle>
+	</view>
+
+
+	<view class="empty-cart" v-else>
+		<image src="/static/cart_empty@2x.png" class="empty-img"></image>
+		<text class="tip-text">空空如也~</text>
 	</view>
 </template>
 
@@ -22,15 +34,25 @@
 	export default {
 		mixins: [badgeMix],
 		data() {
-			return {};
+			return {
+				options: [{
+					text: '删除',
+					style: {
+						backgroundColor: '#C00000'
+					}
+				}]
+			};
 		},
 		methods: {
-			...mapMutations('m_cart', ['updateGoodsState']),
+			...mapMutations('m_cart', ['updateGoodsState', 'updateGoodsCount', 'removeGoodsById']),
 			radioChangeHandler(e) {
 				this.updateGoodsState(e)
 			},
 			numberChangeHandler(e) {
-				console.log(e)
+				this.updateGoodsCount(e)
+			},
+			swipeActionClickHandler(goods) {
+				this.removeGoodsById(goods.goods_id)
 			}
 		},
 		computed: {
@@ -50,6 +72,36 @@
 
 		.cart-title-text {
 			margin-left: 10px;
+		}
+	}
+
+	.goods-item {
+		width: 750rpx;
+		box-sizing: border-box;
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
+	}
+
+	.cart-container {
+		padding-bottom: 50px;
+	}
+
+	.empty-cart {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding-top: 150px;
+
+		.empty-img {
+			width: 90px;
+			height: 90px;
+		}
+
+		.tip-text {
+			font-size: 12px;
+			color: gray;
+			margin-top: 15px;
 		}
 	}
 </style>
