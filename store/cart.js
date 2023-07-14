da9ef2d674ce8ade979ae3292290424b4ee2e6d8
kolibri <vigilanteovo@gmail.com>
2023-07-14 09:40:11 +0800

    add:商品相关

--- store/cart.js
+++ store/cart.js
@@ -28,6 +28,22 @@ export default {
 				findResult.goods_state = goods.goods_state
 				this.commit('m_cart/saveToStorage')
 			}
+		},
+		updateGoodsCount(state, goods) {
+			const findResult = state.cart.find(item => item.goods_id === goods.goods_id)
+
+			if (findResult) {
+				findResult.goods_count = goods.goods_count
+				this.commit('m_cart/saveToStorage')
+			}
+		},
+		removeGoodsById(state, goods_id) {
+			state.cart = state.cart.filter(item => item.goods_id !== goods_id)
+			this.commit('m_cart/saveToStorage')
+		},
+		updateAllGoodsState(state, newState) {
+			state.cart.forEach(x => x.goods_state = newState)
+			this.commit('m_cart/saveToStorage')
 		}
 	},
 
@@ -36,6 +52,14 @@ export default {
 			let counter = 0
 			state.cart.forEach(goods => counter += goods.goods_count)
 			return counter
+		},
+		checkedCount(state) {
+			return state.cart.filter(item => item.goods_state).reduce((total, item) => total += item.goods_count, 0)
+		},
+		checkedGoodsAmount(state) {
+			return state.cart.filter(item => item.goods_state)
+				.reduce((total, item) => total += item.goods_count * item.goods_price, 0)
+				.toFixed(2)
 		}
 	}
 }
